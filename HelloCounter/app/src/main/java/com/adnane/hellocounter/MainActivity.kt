77e50983ada71c304428b8608c8a1bf6172f581d
Adnane Kes <tangerinoh40@gmail.com>
2025-10-07 13:20:39 +0100

    done

--- HelloCounter/app/src/main/java/com/adnane/hellocounter/MainActivity.kt
+++ HelloCounter/app/src/main/java/com/adnane/hellocounter/MainActivity.kt
@@ -3,45 +3,62 @@ package com.adnane.hellocounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adnane.hellocounter.ui.DireBonjourSection
 import com.adnane.hellocounter.ui.theme.HelloCounterTheme
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import com.adnane.hellocounter.ui.CompteurSection
+import com.adnane.hellocounter.ui.DireBonjourSection
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             HelloCounterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DireBonjourSection()
+            CompteurSection()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, widthDp = 360)
 @Composable
-fun GreetingPreview() {
+fun PreviewMainScreen() {
     HelloCounterTheme {
-        Greeting("Android")
+        CompteurSection()
     }
 }
