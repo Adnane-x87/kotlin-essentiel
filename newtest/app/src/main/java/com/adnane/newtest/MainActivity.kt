84e1e3e4da4ff408746e2adcf9521649c66a55f0
Adnane Kes <tangerinoh40@gmail.com>
2025-10-10 10:34:58 +0100

    new commit

--- newtest/app/src/main/java/com/adnane/newtest/MainActivity.kt
+++ newtest/app/src/main/java/com/adnane/newtest/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewTestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                        FirstNameScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewTestTheme {
-        Greeting("Android")
     }
 }
